e32d85f1d927484b4bdec7d48f2ff3ba459de0a1
Naman Kumar <59838992+namankr85100@users.noreply.github.com>
2020-09-23 15:51:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+//app.listen(3000);
+
+// view engine
+app.set('view engine', 'ejs');
+
+var port = process.env.PORT || 3000;
+const dbURI = "mongodb+srv://naman:8510887827@cluster0.maucb.mongodb.net/node-auth";
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(port))
+  .catch((err) => console.log(err));
+
+//app.listen(3000);
+
+  // routes
+app.get('*', checkUser);
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', requireAuth, (req, res) => res.render('smoothies'));
+app.use(authRoutes);
